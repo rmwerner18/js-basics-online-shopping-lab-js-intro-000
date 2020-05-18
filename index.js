7862625327628ad1562bfaa04e7d57a99cbb3d00
Ryan Werner <rmwerner@umich.edu>
2020-05-18 20:51:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -39,9 +39,9 @@ function removeFromCart(item) {
 if (cart.includes(`${item}`) === false) {return `That item is not in your cart.`}
 }
 
-function placeOrder(cardNumber) {
+function placeOrder(cardNumber) 
   {if (cardNumber === undefined ) {return `Sorry, we don't have a credit card on file for you.`}
   else
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`}
-  cart.splice(0, cart.length)
+    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+     var cart = []
 }
