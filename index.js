5c5002db5e311c8396261f8ede0edc72d6b80916
Ryan Werner <rmwerner@umich.edu>
2020-05-18 20:14:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,19 +10,33 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+var item = { itemName: `${item}`, itemPrice: Math.floor(Math.random() * 100) + 1 };
+cart.push(item);
+return `${item.itemName} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+var newArray = []
+for (let i = 0; i < cart.length; i++) {
+  newArray.push(` ${cart[i].itemName} at $${cart[i].itemPrice}`)}
+  if (cart.length === 0) {return `Your shopping cart is empty.`}
+  else if (cart.length === 1) {return `In your cart, you have${newArray}.`}
+  else {return `In your cart, you have${newArray.slice(0, newArray.length - 1)}, and${newArray.slice(-1)}.`}
 }
 
 function total() {
-  // write your code here
+  var sum = 0
+  for (let i = 0; i < cart.length; i++) {
+    sum = sum + cart[i].itemPrice
+  }
+  return sum
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for (let i = 0; i < cart.length; i++)
+  {if (`${item}` === cart[i].itemName) {return cart.splice(i, 1)}
+}
+if (removeFromCart(`${item}`) === `undefined`) {return `That item is not in cour cart.`}
 }
 
 function placeOrder(cardNumber) {
