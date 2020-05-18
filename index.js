46491ef29cdb75d0237cb4461298a89e38efb4d9
Ryan Werner <rmwerner@umich.edu>
2020-05-18 20:40:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (cardNumber === undefined ) {return `Sorry, we don't have a credit card on file for you.`}
-  else 
+  else
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
 }
 
