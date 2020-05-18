23e3f87c59eefe9dd78b4e247f6537619dfcc6d2
Ryan Werner <rmwerner@umich.edu>
2020-05-18 21:39:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -43,5 +43,5 @@ function placeOrder(cardNumber) {
   {if (cardNumber === undefined) {return `Sorry, we don't have a credit card on file for you.`}
   else {return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`}}
   if (placeOrder(cardNumber) === `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
-  {var cart = []}
+  {return (var cart = [])}
 }
