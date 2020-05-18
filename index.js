a025c70b2f038e4a126613ecf472d9a58cec567d
Ryan Werner <rmwerner@umich.edu>
2020-05-18 20:45:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -35,7 +35,6 @@ function total() {
 function removeFromCart(item) {
   for (let i = 0; i < cart.length; i++)
   {if (`${item}` === cart[i].itemName) {return cart.splice(i, 1)}
-  else {return `That item is not in your cart.`}
 }}
 
 function placeOrder(cardNumber) {
